fc78d2bac1809bd6e0f1b4e902bc64895e42bcd1
torstenboettjer <torsten.boettjer@gmail.com>
2026-05-12 19:48:08 +0200

    UPDATE: Resolve regions issues in output code.

--- data/app/graphql.js
+++ data/app/graphql.js
@@ -7,7 +7,7 @@ window.VIEWS = {
     icon: "M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z",
     query: `{ subscription { tenant location name stage } }`,
     node: "subscription",
-    columns: ["tenant", "location", "solution", "stage"],
+    columns: ["organisation", "location", "solution", "stage"],
   },
   accounts: {
     title: "Account",
