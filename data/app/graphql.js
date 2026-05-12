bcf68b271d948b1069344d5bb7d7a0aef724296c
torstenboettjer <torsten.boettjer@gmail.com>
2026-05-12 19:11:27 +0200

    UPDATE: Resolve regions issues in output code.

--- data/app/graphql.js
+++ data/app/graphql.js
@@ -2,13 +2,6 @@
 // Maps UI views to GraphQL queries based on AWS Network Hub models
 
 window.VIEWS = {
-  providers: {
-    title: "Region",
-    icon: "M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z",
-    query: `{ site { location region endpoint availability_zone } }`,
-    node: "site",
-    columns: ["name", "region", "endpoints", "availability zones"],
-  },
   subscriptions: {
     title: "Subscription",
     icon: "M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z",
@@ -16,8 +9,15 @@ window.VIEWS = {
     node: "subscription",
     columns: ["name", "tenant", "stage"],
   },
+  providers: {
+    title: "Region",
+    icon: "M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z",
+    query: `{ site { location region endpoint availability_zone } }`,
+    node: "site",
+    columns: ["name", "region", "endpoints", "availability zones"],
+  },
   logins: {
-    title: "Operator",
+    title: "Identity",
     icon: "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z",
     query: `{ login { name function description } }`,
     node: "login",
