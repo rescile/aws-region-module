3b72f1aad33cd0683c3ff4875d43d999d877f199
torstenboettjer <torsten.boettjer@gmail.com>
2026-05-12 19:06:29 +0200

    UPDATE: Resolve regions issues in output code.

--- data/app/graphql.js
+++ data/app/graphql.js
@@ -17,11 +17,11 @@ window.VIEWS = {
     columns: ["name", "tenant", "stage"],
   },
   routers: {
-    title: "Network & Transit Routers",
+    title: "Gateways",
     icon: "M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z",
-    query: `{ router { name function site network { node { cidr} } } }`,
+    query: `{ gateway { original_name function network { node { cidr} } } }`,
     node: "router",
-    columns: ["name", "function", "site", "network"],
+    columns: ["name", "function", "network"],
   },
   regions: {
     title: "Endpoint Regions",
