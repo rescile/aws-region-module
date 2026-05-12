368bd68c211bec0c7bf367b4a35f1a57ef4599e7
torstenboettjer <torsten.boettjer@gmail.com>
2026-05-12 19:18:21 +0200

    UPDATE: Resolve regions issues in output code.

--- data/app/graphql.js
+++ data/app/graphql.js
@@ -9,6 +9,13 @@ window.VIEWS = {
     node: "subscription",
     columns: ["name", "tenant", "stage"],
   },
+  accounts: {
+    title: "Account",
+    icon: "M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z",
+    query: `{ account { name function } }`,
+    node: "account",
+    columns: ["name", "function"],
+  },
   providers: {
     title: "Region",
     icon: "M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9",
@@ -36,13 +43,6 @@ window.VIEWS = {
     query: `{ region { name function pid endpoint } }`,
     node: "region",
     columns: ["name", "function", "pid", "endpoint"],
-  },
-  accounts: {
-    title: "Account",
-    icon: "M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z",
-    query: `{ account { name function } }`,
-    node: "account",
-    columns: ["name", "function"],
   },
 };
 
