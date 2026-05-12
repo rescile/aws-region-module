97963ed92dc378d4f55bf22865e642903a32bfb5
torstenboettjer <torsten.boettjer@gmail.com>
2026-05-12 18:17:39 +0200

    UPDATE: Resolve regions issues in output code.

--- data/app/graphql.js
+++ data/app/graphql.js
@@ -5,7 +5,7 @@ window.VIEWS = {
   providers: {
     title: "Cloud Region",
     icon: "M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z",
-    query: `{ site { location region endpoint availability_zone } }`,
+    query: `{ provider { location region endpoint availability_zone } }`,
     node: "provider",
     columns: ["name", "region", "endpoints", "Availability Zones"],
   },
